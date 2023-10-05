9fa6781dfa68c935b41c1cdb430b476279d311a0
owen <wavefunctionc@gmail.com>
2023-10-05 16:50:36 +0900

    MessagePublisher - 간단한 Scheduled Send Message 기능 작성

--- src/main/java/com/generator/service/MessagePublisher.java
+++ src/main/java/com/generator/service/MessagePublisher.java
@@ -1,134 +1,65 @@
 package com.generator.service;
 
-import com.generator.dto.MessageDto;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeoutException;
+import java.util.Objects;
 
 @Slf4j
 @Service
-@Transactional
-@RequiredArgsConstructor
 public class MessagePublisher {
+    @Autowired
+    @Qualifier("template1")
+    private RabbitTemplate template1;
+    @Autowired
+    @Qualifier("template2")
+    private RabbitTemplate template2;
+    @Autowired
+    @Qualifier("template3")
+    private RabbitTemplate template3;
+    @Autowired
+    @Qualifier("template4")
+    private RabbitTemplate template4;
 
-    private static final String QUEUE_NAME = "test_queue";
-    private static final String[] EXCHANGE_NAMES = {"exchange1", "exchange2", "exchange3", "exchange4"};
-    private final List<MessageDto> messages;
-    private final RabbitTemplate template;
-
-    @Scheduled(fixedRate = 1000)
-    public void publishData1(int instanceNumber) {
-        MessageDto dto = messages.get(instanceNumber);
-        dto.setInstanceNumber(dto.getInstanceNumber() + 1);
-        template.convertAndSend("t1", dto);
+    @Scheduled(fixedDelay = 1000) // 1초마다 실행
+    public void simulate1() {
+        sendData(template1, "ex.one", "one");
     }
 
-    @Scheduled(fixedRate = 1000)
-    public void publishData2(int instanceNumber) {
-        MessageDto dto = messages.get(instanceNumber);
-        dto.setInstanceNumber(dto.getInstanceNumber() + 1);
-        template.convertAndSend("t2", dto);
+    @Scheduled(fixedDelay = 1000) // 1초마다 실행
+    public void simulate2() {
+        sendData(template2, "ex.two", "two");
     }
 
-    @Scheduled(fixedRate = 1000)
-    public void publishData3(int instanceNumber) {
-        MessageDto dto = messages.get(instanceNumber);
-        dto.setInstanceNumber(dto.getInstanceNumber() + 1);
-        template.convertAndSend("t3", dto);
+    @Scheduled(fixedDelay = 1000) // 1초마다 실행
+    public void simulate3() {
+        sendData(template3, "ex.three", "three");
     }
 
-    @Scheduled(fixedRate = 1000)
-    public void publishData4(int instanceNumber) {
-        MessageDto dto = messages.get(instanceNumber);
-        dto.setInstanceNumber(dto.getInstanceNumber() + 1);
-        template.convertAndSend("t4", dto);
+    @Scheduled(fixedDelay = 1000) // 1초마다 실행
+    public void simulate4() {
+        sendData(template4, "ex.four", "four");
     }
 
-    private static class ProducerTask implements Runnable {
+    public void sendData(RabbitTemplate template, String exchange, String routingKey) {
+        String server = switch (template.getConnectionFactory().getPort()) {
+            case 5672 -> "1";
+            case 5673 -> "2";
+            case 5674 -> "3";
+            case 5675 -> "4";
+            default -> "";
+        };
 
-        private final int index;
-        private final MessageDto[] messages;
-        private final int[] messageCounts;
-
-        public ProducerTask(int index, MessageDto[] messages, int[] messageCounts) {
-            this.index = index;
-            this.messages = messages;
-            this.messageCounts = messageCounts;
+        try {
+            String message = "Test Message";
+            template.convertAndSend(exchange, routingKey, message);
+            log.info("[Rabbit {}] - 데이터 전송 완료", server);
+        } catch (Exception e) {
+            log.error("[Simulator Error] : {}", e.getMessage());
         }
-
-        @Override
-        public void run() {
-            // RabbitMQ 연결 설정
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setPort(5672);
-            factory.setUsername("guest");
-            factory.setPassword("guest");
-
-            try (Connection connection = factory.newConnection(); Channel channel = connection.createChannel()) {
-                // Exchange 설정
-                for (String exchangeName : EXCHANGE_NAMES) {
-                    channel.exchangeDeclare(exchangeName, "direct");
-
-
-                    // 메시지 발송
-                    for (int i = 0; i < messageCounts[index]; i++) {
-                        MessageDto message = messages[index * 30 + i];
-                        message.setTestNumber(String.valueOf(i + 1));
-
-                        channel.basicPublish(exchangeName, "", MessageProperties.PERSISTENT_TEXT_PLAIN, message.toString().getBytes());
-                    }
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (TimeoutException e) {
-                throw new RuntimeException(e);
-            }
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        // RabbitMQ 연결 설정
-        ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        factory.setPort(5672);
-        factory.setUsername("guest");
-        factory.setPassword("guest");
-
-        // 스레드 풀 생성
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-
-        // 메시지 생성
-        MessageDto[] messages = new MessageDto[120];
-        for (int i = 0; i < messages.length; i++) {
-            messages[i] = new MessageDto(i + 1);
-        }
-
-        // 메시지 분배
-        int[] messageCounts = new int[4];
-        for (int i = 0; i < messages.length; i++) {
-            int index = i % 4;
-            messageCounts[index]++;
-        }
-
-        // 메시지 발송
-        for (int i = 0; i < 4; i++) {
-            executorService.execute(new ProducerTask(i, messages, messageCounts));
-        }
-
-        executorService.shutdown();
     }
 }
