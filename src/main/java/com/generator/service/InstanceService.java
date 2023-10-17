ed368ea4f7a4885a6f7cccf9e46c0fea4e6102b8
spacedustz <spacedustw@gmail.com>
2023-10-17 17:33:24 +0900

    InstanceService 추가 - Random Instance 리스트 반환

---
+++ src/main/java/com/generator/service/InstanceService.java
@@ -0,0 +1,39 @@
+package com.generator.service;
+
+import co.kr.dains.crowd.estimation.common.domain.svc.SvcInstance;
+import co.kr.dains.crowd.estimation.common.repository.svc.SvcInstanceRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class InstanceService {
+    private final SvcInstanceRepository svcInstanceRepository;
+
+    public List<SvcInstance> getRandomInstances() {
+        List<SvcInstance> selectedInstances = new ArrayList<>();
+        List<SvcInstance> instances = svcInstanceRepository.findAll();
+
+        Map<Integer, List<SvcInstance>> groupedInstances = instances.stream().collect(Collectors.groupingBy(it -> it.getSvcCamera().getCameraId()));
+
+        groupedInstances.values().forEach(it -> {
+            if (it.size() >= 2) {
+                int randomIndex = new Random().nextInt(it.size());
+                selectedInstances.add(it.get(randomIndex));
+            }
+        });
+
+//        selectedInstances.stream().map(SvcInstance::getInstanceName).forEach(it -> log.info("랜덤 리스트 로그 : {}", it));
+
+        return selectedInstances;
+    }
+}
+
